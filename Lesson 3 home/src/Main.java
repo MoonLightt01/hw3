e2cec36f2dc50cc640993c09ce226aea66a6970b
MoonLightt01 <byrman.slavik@gmail.com>
2023-05-31 00:29:17 +0300

    Edit onecoof->oneCoof Edit twocoof->twoCoof Edit tempc->tempC

--- Lesson 3 home/src/Main.java
+++ Lesson 3 home/src/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
         System.out.println("\t\t\tДобро пожаловать в конвертер температуры из цельсия в фаренгейт");
         System.out.println("Введите температуру в градусах цельсия:");
-        double tempc = new Scanner(System.in).nextDouble();
-        double onecoof = 1.8;
-        int twocoof = 32;
+        double tempC = new Scanner(System.in).nextDouble();
+        double oneCoof = 1.8;
+        int twoCoof = 32;
 
-        System.out.println("C: " + tempc + " градусов по цельсию\n" + "Это "
-                + (tempc * onecoof + twocoof) + " градусов по фаренгейту");
+        System.out.println("C: " + tempC + " градусов по цельсию\n" + "Это "
+                + (tempC * oneCoof + twoCoof) + " градусов по фаренгейту");
     }
 }
